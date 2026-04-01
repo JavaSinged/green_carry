6433281b9db91f91d2e8a2b82c03cc14e9f8909c
jihoon24 <shinjiwoong1656@gmail.com>
2026-04-01 10:02:16 +0900

    Revert "Update App.jsx"
    This reverts commit 791113fbed05fed403fa3f0fa8134bcf05c09010.

--- creen_proejct_front/src/App.jsx
+++ creen_proejct_front/src/App.jsx
@@ -15,13 +15,19 @@ import UserLayout from './components/layout/mypageSidebar/UserLayout';
 import ManagerLayout from './components/layout/mypageSidebar/ManagerLayout';
 import AdminLayout from './components/layout/mypageSidebar/AdminLayout';
 import UserProfile from './pages/mypage/user/UserProfile';
+import UserInfoEdit from './pages/mypage/user/UserInfoEdit';
+
+import { AuthProvider } from "./context/AuthContext";
+import UserSignup from "./pages/signup/UserSignup";
+import ManagerSignup from "./pages/signup/ManagerSignup";
+import Signup from "./pages/signup/Signup";
 
 const BasicLayout = () => {
   return (
     <>
       <Header />
       <main>
-        <Outlet /> {/* 이 자리에 Home, StoreView 등이 쏙쏙 들어갑니다 */}
+        <Outlet />
       </main>
       <Footer />
     </>
@@ -30,46 +36,50 @@ const BasicLayout = () => {
 
 function App() {
   return (
-    <div>
-      <Routes>
-        {/* 1. 헤더/푸터가 필요 없는 단독 화면 */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/account" element={<Account />} />
+    // 🌟 전체를 AuthProvider로 감쌉니다.
+    <AuthProvider>
+      <div>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/userSignup" element={<UserSignup />} />
+          <Route path="/managerSignup" element={<ManagerSignup />} />
 
-        {/* 2. 헤더/푸터가 무조건 붙어야 하는 화면들 (BasicLayout 안에 넣기) */}
-        <Route element={<BasicLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/storeView" element={<StoreView />} />
-          <Route path="/storeDetail" element={<StoreDetail />} />
+          {/* 2. 헤더/푸터가 무조건 붙어야 하는 화면들 (BasicLayout 안에 넣기) */}
+          <Route element={<BasicLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/storeView" element={<StoreView />} />
+            <Route path="/storeDetail" element={<StoreDetail />} />
 
-          {/* 🧑 일반 유저 마이페이지 */}
-          <Route path="/mypage/user" element={<UserLayout />}>
-            <Route index element={<UserProfile />} />
-            <Route path="/userInfoEdit" element={<UserInfoEdit />} />
-            {/* 기본 화면: 개인정보 수정 */}
-            {/* <Route path="orders" element={<UserOrders />} /> */}
-            {/* /mypage/user/orders */}
-            {/* 필요한 메뉴만큼 Route를 추가하세요 */}
-          </Route>
+            {/* 🧑 일반 유저 마이페이지 */}
+            <Route path="/mypage/user" element={<UserLayout />}>
+              <Route index element={<UserProfile />} />
+              <Route path="userInfoEdit" element={<UserInfoEdit />} />
+              {/* 기본 화면: 개인정보 수정 */}
+              {/* <Route path="orders" element={<UserOrders />} /> */}
+              {/* /mypage/user/orders */}
+              {/* 필요한 메뉴만큼 Route를 추가하세요 */}
 
-          {/* 👨‍🍳 점주 마이페이지 */}
-          <Route path="/mypage/manager" element={<ManagerLayout />}>
-            {/* <Route index element={<ManagerDashboard />} />{' '} */}
-            {/* 기본 화면: 통계 메인 */}
-            {/* <Route path="menus" element={<ManagerMenus />} /> */}
-          </Route>
+              {/* 👨‍🍳 점주 마이페이지 */}
+              <Route path="/mypage/manager" element={<ManagerLayout />}>
+                {/* <Route index element={<ManagerDashboard />} />{' '} */}
+                {/* 기본 화면: 통계 메인 */}
+                {/* <Route path="menus" element={<ManagerMenus />} /> */}
+              </Route>
 
-          {/* 👮 관리자 마이페이지 */}
-          <Route path="/mypage/admin" element={<AdminLayout />}>
-            {/* <Route index element={<AdminMembers />} />{' '} */}
-            {/* 기본 화면: 회원 관리 */}
-            {/* <Route path="stores" element={<AdminStores />} /> */}
-          </Route>
+              {/* 👮 관리자 마이페이지 */}
+              <Route path="/mypage/admin" element={<AdminLayout />}>
+                {/* <Route index element={<AdminMembers />} />{' '} */}
+                {/* 기본 화면: 회원 관리 */}
+                {/* <Route path="stores" element={<AdminStores />} /> */}
+              </Route>
 
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </div>
+              <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes >
+      </div >
+    </AuthProvider >
   );
 }
 
