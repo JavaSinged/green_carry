ff0a4bf4b6f9f677db136d45bbebec27d4d72c45
choiseunghwan34 <chltmdghks7894@gmail.com>
2026-04-07 13:58:23 +0900

    Update PaymentPage.jsx

--- creen_proejct_front/src/pages/order/PaymentPage.jsx
+++ creen_proejct_front/src/pages/order/PaymentPage.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import styles from './PaymentPage.module.css';
-import RoomIcon from '@mui/icons-material/Room';
-import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
-import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
-import { useNavigate } from 'react-router-dom';
-import useCartStore from '../../store/useCartStore';
-import useAccountStore from '../../store/accountStore';
-import axios from 'axios';
-import { loadTossPayments } from '@tosspayments/tosspayments-sdk';
+import React, { useState } from "react";
+import styles from "./PaymentPage.module.css";
+import RoomIcon from "@mui/icons-material/Room";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
+import { useNavigate } from "react-router-dom";
+import useCartStore from "../../store/useCartStore";
+import useAccountStore from "../../store/accountStore";
+import axios from "axios";
+import { loadTossPayments } from "@tosspayments/tosspayments-sdk";
 
 const PaymentPage = () => {
   const { superTotalPrice, deliveryPrice, setUsingEcoPoint } = useCartStore();
   const navigate = useNavigate();
-  const [storeRequest, setStoreRequest] = useState('');
-  const [deliveryRequest, setDeliveryRequest] = useState('');
+  const [storeRequest, setStoreRequest] = useState("");
+  const [deliveryRequest, setDeliveryRequest] = useState("");
   const [ecoPoint, setEcoPoint] = useState(0);
   const [reducedCarbon, setReducedCarbon] = useState(300);
   const [itemPrice, setItemPrice] = useState(superTotalPrice);
@@ -26,7 +26,7 @@ const PaymentPage = () => {
   const totalCarbon =
     cartList.reduce((sum, item) => sum + item.carbonSaved, 0) + deliveryCarbon;
   const { setSuperTotalPrice, setDeilveryPrice } = useCartStore();
-  const memberId = localStorage.getItem('memberId');
+  const memberId = localStorage.getItem("memberId");
   const storeId = useCartStore((state) => state.storeId);
   const [payInfo, setPayInfo] = useState({
     totalPrice: totalPrice,
@@ -54,11 +54,12 @@ const PaymentPage = () => {
     storeId: storeId, // 현재 상점 ID
     usedPoint: ecoPoint,
     deliveryType: deliveryType,
+    reviewStatus: 0,
     items: cartList.map((item) => ({
       menuId: Number(item.menuId),
       quantity: item.quantity,
       price: item.unitPrice,
-      optionString: item.options.map((o) => o.optionName).join(',') || '',
+      optionString: item.options.map((o) => o.optionName).join(",") || "",
     })),
     totalPrice: totalPrice,
     getPoint: totalCarbon,
@@ -119,9 +120,9 @@ const PaymentPage = () => {
           : `${cartList[0].name}`;
 
       await payment.requestPayment({
-        method: 'CARD',
+        method: "CARD",
         amount: {
-          currency: 'KRW',
+          currency: "KRW",
           value: totalPrice,
         },
         orderId: `ORDER_${savedOrderId}_${Date.now()}`,
@@ -131,9 +132,9 @@ const PaymentPage = () => {
         customerName: memberId,
       });
     } catch (error) {
-      console.error('결제 준비 중 에러 발생:', error);
+      console.error("결제 준비 중 에러 발생:", error);
       console.log(error.response?.data || error.message); // 에러 내용 더 자세히 보기
-      alert('주문 처리 중 오류가 발생했습니다.');
+      alert("주문 처리 중 오류가 발생했습니다.");
     }
   };
   // const callAxios = () => {
@@ -145,41 +146,41 @@ const PaymentPage = () => {
   //     .catch((err) => {});
   // };
   return (
-    <div className={styles['payment-page']}>
-      <main className={styles['payment-main']}>
-        <div className={styles['payment-top-text']}>
+    <div className={styles["payment-page"]}>
+      <main className={styles["payment-main"]}>
+        <div className={styles["payment-top-text"]}>
           <button
-            className={styles['back-btn']}
+            className={styles["back-btn"]}
             onClick={() => {
-              navigate('/shoppingCart');
+              navigate("/shoppingCart");
             }}
           >
-            {' '}
+            {" "}
             장바구니로 돌아가기
           </button>
           <p>주문 정보를 확인하고 결제를 완료하세요</p>
         </div>
 
-        <div className={styles['payment-content']}>
+        <div className={styles["payment-content"]}>
           {/* 왼쪽 영역 */}
-          <section className={styles['payment-left']}>
-            <div className={styles['info-card']}>
-              <div className={styles['card-title-row']}>
-                <div className={styles['card-title-left']}>
+          <section className={styles["payment-left"]}>
+            <div className={styles["info-card"]}>
+              <div className={styles["card-title-row"]}>
+                <div className={styles["card-title-left"]}>
                   <RoomIcon />
                   <h2>배송정보</h2>
                 </div>
                 <button
-                  className={styles['text-btn']}
+                  className={styles["text-btn"]}
                   onClick={() => {
-                    navigate('/changeAddr');
+                    navigate("/changeAddr");
                   }}
                 >
                   주소 변경
                 </button>
               </div>
 
-              <div className={styles['form-group']}>
+              <div className={styles["form-group"]}>
                 <label>배송 주소</label>
                 <input
                   type="text"
@@ -188,21 +189,21 @@ const PaymentPage = () => {
                 />
               </div>
 
-              <div className={styles['form-group']}>
+              <div className={styles["form-group"]}>
                 <label>연락처</label>
                 <input type="text" value="010-0000-0000" readOnly />
               </div>
             </div>
 
-            <div className={styles['info-card']}>
-              <div className={styles['card-title-row']}>
-                <div className={styles['card-title-left']}>
+            <div className={styles["info-card"]}>
+              <div className={styles["card-title-row"]}>
+                <div className={styles["card-title-left"]}>
                   <ChatBubbleIcon></ChatBubbleIcon>
                   <h2>요청사항</h2>
                 </div>
               </div>
 
-              <div className={styles['form-group']}>
+              <div className={styles["form-group"]}>
                 <label>가게 요청사항</label>
                 <input
                   type="text"
@@ -212,9 +213,9 @@ const PaymentPage = () => {
                 />
               </div>
 
-              <div className={styles['form-group']}>
+              <div className={styles["form-group"]}>
                 <label>배달 요청사항</label>
-                <div className={styles['select-wrap']}>
+                <div className={styles["select-wrap"]}>
                   <select
                     value={deliveryRequest}
                     onChange={(e) => setDeliveryRequest(e.target.value)}
@@ -228,20 +229,20 @@ const PaymentPage = () => {
               </div>
             </div>
 
-            <div className={styles['info-card']}>
-              <div className={styles['card-title-row']}>
-                <div className={styles['card-title-left']}>
+            <div className={styles["info-card"]}>
+              <div className={styles["card-title-row"]}>
+                <div className={styles["card-title-left"]}>
                   <CardGiftcardIcon></CardGiftcardIcon>
                   <h2>에코 포인트 사용 및 적립</h2>
                 </div>
               </div>
 
-              <div className={styles['form-group']}>
+              <div className={styles["form-group"]}>
                 <label>에코 포인트 사용</label>
-                <div className={styles['point-row']}>
+                <div className={styles["point-row"]}>
                   <input
                     type="text"
-                    value={ecoPoint === 0 ? '' : ecoPoint}
+                    value={ecoPoint === 0 ? "" : ecoPoint}
                     onChange={(e) => {
                       const value = e.target.value;
 
@@ -256,7 +257,7 @@ const PaymentPage = () => {
 
                       // 빈값이면 0 처리
                       const num =
-                        value === '' ? 0 : Math.min(Number(value), maxPoint);
+                        value === "" ? 0 : Math.min(Number(value), maxPoint);
 
                       setEcoPoint(num);
                       setPayInfo((prev) => ({
@@ -267,7 +268,7 @@ const PaymentPage = () => {
                     }}
                   />
                   <button
-                    className={styles['point-cancel-btn']}
+                    className={styles["point-cancel-btn"]}
                     onClick={() => {
                       setEcoPoint(0);
                     }}
@@ -275,12 +276,12 @@ const PaymentPage = () => {
                     사용취소
                   </button>
                 </div>
-                <p className={styles['point-desc']}>
+                <p className={styles["point-desc"]}>
                   보유 : {availableEcoPoint.toLocaleString()} 점(원)
                 </p>
               </div>
 
-              <div className={styles['form-group']}>
+              <div className={styles["form-group"]}>
                 <label>이번 주문으로 받을 에코포인트</label>
                 <input
                   type="text"
@@ -292,42 +293,42 @@ const PaymentPage = () => {
           </section>
 
           {/* 오른쪽 */}
-          <aside className={styles['payment-right']}>
-            <div className={styles['summary-card']}>
-              <h2 className={styles['summary-title']}>결제 정보</h2>
+          <aside className={styles["payment-right"]}>
+            <div className={styles["summary-card"]}>
+              <h2 className={styles["summary-title"]}>결제 정보</h2>
 
-              <div className={styles['price-list']}>
-                <div className={styles['price-row']}>
+              <div className={styles["price-list"]}>
+                <div className={styles["price-row"]}>
                   <span>상품합계</span>
                   <span>{itemPrice.toLocaleString()}원</span>
                 </div>
-                <div className={styles['price-row']}>
+                <div className={styles["price-row"]}>
                   <span>배달비</span>
                   <span>{deliveryPrice.toLocaleString()}원</span>
                 </div>
-                <div className={styles['price-row']}>
+                <div className={styles["price-row"]}>
                   <span>에코포인트 사용</span>
                   <span>-{ecoPoint}P</span>
                 </div>
               </div>
 
-              <div className={styles['total-row']}>
+              <div className={styles["total-row"]}>
                 <span>최종 결제 금액</span>
                 <strong>{totalPrice}원</strong>
               </div>
 
-              <div className={styles['carbon-card']}>
-                <p className={styles['carbon-title']}>이 주문의 탄소 절감</p>
-                <strong className={styles['carbon-value']}>
+              <div className={styles["carbon-card"]}>
+                <p className={styles["carbon-title"]}>이 주문의 탄소 절감</p>
+                <strong className={styles["carbon-value"]}>
                   {totalCarbon.toLocaleString()}g
                 </strong>
-                <p className={styles['carbon-desc']}>
+                <p className={styles["carbon-desc"]}>
                   이번 주문으로 나무 가지 하나를 틔웠습니다!
                 </p>
               </div>
 
               <button
-                className={styles['pay-btn']}
+                className={styles["pay-btn"]}
                 onClick={() => {
                   handlePayment();
                   console.log(orderData);
@@ -338,7 +339,7 @@ const PaymentPage = () => {
                 {totalPrice.toLocaleString()}원 결제하기
               </button>
 
-              <p className={styles['pay-notice']}>
+              <p className={styles["pay-notice"]}>
                 결제 진행 시 주문이 확정되며
                 <br />
                 취소는 가게 승인 전까지 가능합니다.
