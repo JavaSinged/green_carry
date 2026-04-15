357de13c3fbc186b4cd1879ad58212097185d196
jihoon24 <shinjiwoong1656@gmail.com>
2026-04-15 16:04:43 +0900

    main.jsx수정

--- creen_proejct_front/src/main.jsx
+++ creen_proejct_front/src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 // router setting
 import { BrowserRouter } from "react-router-dom";
@@ -9,5 +10,6 @@ createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
     <Analytics />
+    <SpeedInsights />
   </BrowserRouter>,
 );
