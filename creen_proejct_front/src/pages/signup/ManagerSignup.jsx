16e067b85833e160c571d424adb573115e324941
damso99 <stk9124@gmail.com>
2026-04-16 14:31:24 +0900

    1

--- creen_proejct_front/src/pages/signup/ManagerSignup.jsx
+++ creen_proejct_front/src/pages/signup/ManagerSignup.jsx
@@ -4,7 +4,7 @@ import "react-calendar/dist/Calendar.css";
 import axios from "axios";
 import Swal from "sweetalert2";
 import useEcoEffects from "../../hooks/useEcoEffects";
-import "./UserSignup.css";
+import "./UserSignup.module.css";
 
 const ManagerSignup = () => {
   // 🌟 leafData(나뭇잎 효과) 추가
