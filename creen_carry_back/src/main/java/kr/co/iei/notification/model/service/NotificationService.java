63608a86b4ebd24468941a264eed05f9c8c975fc
taehyun.park <ads10257@naver.com>
2026-04-20 15:31:18 +0900

    Update NotificationService.java

--- creen_carry_back/src/main/java/kr/co/iei/notification/model/service/NotificationService.java
+++ creen_carry_back/src/main/java/kr/co/iei/notification/model/service/NotificationService.java
@@ -1,5 +1,6 @@
 package kr.co.iei.notification.model.service; // 이 경로가 실제 폴더 구조와 맞아야 합니다!
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import java.io.IOException;
@@ -13,8 +14,13 @@ public class NotificationService {
 	private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
 
 	public SseEmitter subscribe(String memberId) {
-		SseEmitter emitter = new SseEmitter(60 * 60 * 1000L);
+		// 1. 이미 연결된 정보가 있다면 종료시키고 새로 생성
+		if (emitters.containsKey(memberId)) {
+			emitters.get(memberId).complete();
+			emitters.remove(memberId);
+		}
 
+		SseEmitter emitter = new SseEmitter(60 * 60 * 1000L);
 		emitters.put(memberId, emitter);
 
 		emitter.onCompletion(() -> emitters.remove(memberId));
@@ -42,5 +48,17 @@ public class NotificationService {
 				emitters.remove(memberId);
 			}
 		}
+	}
+
+	@Scheduled(fixedRate = 30000)
+	public void sendHeartbeat() {
+		emitters.forEach((memberId, emitter) -> {
+			try {
+				emitter.send(SseEmitter.event().name("ping") // 이벤트 이름을 "ping" 혹은 "heartbeat"로
+						.data("heartbeat"));
+			} catch (IOException e) {
+				emitters.remove(memberId);
+			}
+		});
 	}
 }
