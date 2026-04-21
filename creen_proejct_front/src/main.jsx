90ccbda215975de00d08e254a23b439d61860768
damso99 <stk9124@gmail.com>
2026-04-21 14:01:23 +0900

    버튼 중복 클릭방지

--- creen_proejct_front/src/main.jsx
+++ creen_proejct_front/src/main.jsx
@@ -2,9 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import axios from "axios";
+import { installMutationGuard } from "./utils/mutationGuard";
 
 // router setting
 import { BrowserRouter } from "react-router-dom";
+
+installMutationGuard(axios);
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
